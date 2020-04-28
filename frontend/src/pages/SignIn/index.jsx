0454773b2dfc6737329c6793c953f5b7c8df04ee
Wellington Reis <wellington.costa.97@gmail.com>
2020-04-27 23:17:41 -0300

    Adicionado o Unform formularios

--- frontend/src/pages/SignIn/index.jsx
+++ frontend/src/pages/SignIn/index.jsx
@@ -1,34 +1,57 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { Form } from '@unform/web';
+import Input from '../../components/Input';
 
-export default class Login extends Component {
-    render() {
-        return (
-            <form>
-                <h3>Sign In</h3>
+export default function Login() {
+	function handleSubmit(data) {
+		console.log(data);
+		// Return is a JSON, like this { data: 'dataHere', moreData: 'moreDataHere'}
+	}
 
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
+	return (
+		<Form onSubmit={handleSubmit}>
+			<h3>Sign In</h3>
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
+			<div className='form-group'>
+				<label>Email address</label>
+				<Input
+					name='email'
+					type='email'
+					className='form-control'
+					placeholder='Enter email'
+				/>
+			</div>
 
-                <div className="form-group">
-                    <div className="custom-control custom-checkbox">
-                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
-                        <label className="custom-control-label" htmlFor="customCheck1">Remember me</label>
-                    </div>
-                </div>
+			<div className='form-group'>
+				<label>Password</label>
+				<Input
+					name='password'
+					type='password'
+					className='form-control'
+					placeholder='Enter password'
+				/>
+			</div>
 
-                <button type="submit" className="btn btn-primary btn-block">Submit</button>
-                <p className="forgot-password text-right">
-                    Forgot <a href="#">password?</a>
-                </p>
-            </form>
-        );
-    }
+			<div className='form-group'>
+				<div className='custom-control custom-checkbox'>
+					<input
+						type='checkbox'
+						className='custom-control-input'
+						id='customCheck1'
+					/>
+					<label className='custom-control-label' htmlFor='customCheck1'>
+						Remember me
+					</label>
+				</div>
+			</div>
+
+			<button type='submit' className='btn btn-primary btn-block'>
+				Submit
+			</button>
+			<p className='forgot-password text-right'>
+				Forgot <a href='#'>password?</a>
+			</p>
+		</Form>
+	);
 }
--- frontend/src/pages/SignUp/index.jsx
+++ frontend/src/pages/SignUp/index.jsx
@@ -1,37 +1,64 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { Form } from '@unform/web';
+import Input from '../../components/Input';
 
-export default class SignUp extends Component {
-    render() {
-        return (
-            <form>
-                <h3>Sign Up</h3>
+export default function SignUp() {
+	function handleSubmit(data) {
+		console.log(data);
+		// Return is a JSON, like this { data: 'dataHere', moreData: 'moreDataHere'}
+	}
 
-                <div className="form-group">
-                    <label>First name</label>
-                    <input type="text" className="form-control" placeholder="First name" />
-                </div>
+	return (
+		<Form onSubmit={handleSubmit}>
+			<h3>Sign Up</h3>
 
-                <div className="form-group">
-                    <label>Last name</label>
-                    <input type="text" className="form-control" placeholder="Last name" />
-                </div>
+			<div className='form-group'>
+				<label>First name</label>
+				<input
+					type='text'
+					name='firstName'
+					className='form-control'
+					placeholder='First name'
+				/>
+			</div>
 
-                <div className="form-group">
-                    <label>Email address</label>
-                    <input type="email" className="form-control" placeholder="Enter email" />
-                </div>
+			<div className='form-group'>
+				<label>Last name</label>
+				<input
+					type='text'
+					name='lastName'
+					className='form-control'
+					placeholder='Last name'
+				/>
+			</div>
 
-                <div className="form-group">
-                    <label>Password</label>
-                    <input type="password" className="form-control" placeholder="Enter password" />
-                </div>
+			<div className='form-group'>
+				<label>Email address</label>
+				<input
+					type='email'
+					name='email'
+					className='form-control'
+					placeholder='Enter email'
+				/>
+			</div>
 
-                <button type="submit" className="btn btn-primary btn-block">Sign Up</button>
-                <p className="forgot-password text-right">
-                    Already registered <a href="#">sign in?</a>
-                </p>
-            </form>
-        );
-    }
+			<div className='form-group'>
+				<label>Password</label>
+				<input
+					type='password'
+					name='password'
+					className='form-control'
+					placeholder='Enter password'
+				/>
+			</div>
+
+			<button type='submit' className='btn btn-primary btn-block'>
+				Sign Up
+			</button>
+			<p className='forgot-password text-right'>
+				Already registered <a href='#'>sign in?</a>
+			</p>
+		</Form>
+	);
 }
