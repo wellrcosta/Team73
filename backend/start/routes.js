ac8168647f19b0226344a7220e29c063e59edc13
Silas Almeida <silasadssn@gmail.com>
2020-05-02 15:03:55 -0300

    task #0010

--- backend/start/routes.js
+++ backend/start/routes.js
@@ -13,5 +13,14 @@ Route.get('/', () => {
 	return { greeting: 'Hello world in JSON' };
 });
 Route.post('/populate', 'Populate.Do');
-Route.post('/users', 'UserController.create');
 Route.post('/sessions', 'SessionController.create');
+Route.group(() => {
+	Route.resource('/user', 'CustomerController')
+		.apiOnly()
+		.only(['store', 'update', 'destroy']);
+});
+Route.group(() => {
+	Route.resource('/seller', 'SellerController')
+		.apiOnly()
+		.only(['store', 'update', 'destroy']);
+});
